e89da3674631674f4bc6282caf36ae6ecd24970e
Alan fabian Castelli <castelli_alanunlp@hotmail.com>
2017-08-29 18:15:37 -0300

    u proyecto cental creado, con todo lo echo hasta el momento

---
+++ Auto Final/Auto Final.ino
@@ -0,0 +1,15 @@
+void setup()
+{
+
+	Serial.begin(9600);
+
+}
+
+void loop()
+{
+	//tomo tecla de aplicacion bluetooth
+	//modifico la maquina de estados
+	//Acciono segun corresponda
+	//Actualizo
+
+}
