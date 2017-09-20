d3fda4faa6ae77035c80bd4f2b7fb7cc10d88d77
aCastelli95 <castelli_alanunlp@hotmail.com>
2017-09-19 23:26:12 -0300

    Aplicacion con la lista de dispositivos Bluetooth para el cumpleaños de Ro

---
+++ Feliz_Cumple_Ro/app/src/main/java/com/example/alanfabiancastelli/feliz_cumple_ro/autoControl.java
@@ -0,0 +1,13 @@
+package com.example.alanfabiancastelli.feliz_cumple_ro;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class autoControl extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auto_control);
+    }
+}
